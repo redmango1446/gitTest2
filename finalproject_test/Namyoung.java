c688fdcb317f06b2aa8c30eb83383353222a751c
Sunghun Kim <redmango1446@gmail.com>
2022-03-06 22:53:25 +0900

    아오오

--- finalproject_test/Namyoung.java
+++ finalproject_test/Namyoung.java
@@ -3,6 +3,6 @@ package finalproject_test;
 public class Namyoung {
 	public static void main(String[] args) {
 		System.out.println("첫번째 ㅎㅇ여");
-		System.out.println("2번째 ㅎㅇ여");
+		System.out.println("두번째 ㅎㅇ여");
 	}
 }
